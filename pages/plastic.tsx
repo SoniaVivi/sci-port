f8413ac7d0100ae6230cc9f7e390234425bb975e
Siona Vivian <SVivn@proton.me>
2025-11-16 16:02:48 -0800

    plastic: add icons, create initial secondPost

--- pages/plastic.tsx
+++ pages/plastic.tsx
@@ -1,6 +1,10 @@
 import FeaturedContainer from "@/components/FeaturedContainer";
+import Image from "next/image";
 import FirstPost from "@/text/plastic/firstPost.mdx";
+import SecondPost from "@/text/plastic/secondPost.mdx";
 import koiKoiImage from "@/public/projects/koiKoi.png";
+import tireIcon from "@/public/icons/tire.png";
+import shirtIcon from "@/public/icons/shirt.png";
 import styles from "@/styles/Plastic.module.scss";
 
 export default function Home() {
@@ -10,8 +14,29 @@ export default function Home() {
         Life is Plastic!
       </h1>
       <h2 className={`${styles.heading} ${styles.typewriter}`}>Embrace It!</h2>
+      <div className={`${styles.iconContainer} flex center`}>
+        <Image
+          className="clickable"
+          width="50"
+          height="50"
+          src={tireIcon}
+          alt={"Tire"}
+          placeholder="blur"
+        />
+        <Image
+          className="clickable"
+          width="50"
+          height="50"
+          src={shirtIcon}
+          alt={"Shirt"}
+          placeholder="blur"
+        />
+      </div>
       <FeaturedContainer imageLink={koiKoiImage} alt="Plastic">
         <FirstPost />
+      </FeaturedContainer>
+      <FeaturedContainer imageLink={koiKoiImage} alt="Plastic">
+        <SecondPost />
       </FeaturedContainer>
     </div>
   );
